eaad8fd3d6a5f570517b444fb74e83e1bedab8d8
VINNE7 <marcusv.ribas7@gmail.com>
2024-11-05 00:49:29 -0300

    feat: create access token guard for future auth implementation

---
+++ src/common/guards/access-token.guard.ts
@@ -0,0 +1,45 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpStatus,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+import jwtConfig from '../config/jwt.config';
+
+@Injectable()
+export class AccessTokenGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        this.jwtConfiguration,
+      );
+      request['user'] = payload;
+    } catch (err) {
+      throw new UnauthorizedException(HttpStatus.UNAUTHORIZED, err);
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
