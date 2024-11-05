7ec08482e35d7df1b83528e04a69b2750fb4f2c3
VINNE7 <marcusv.ribas7@gmail.com>
2024-11-05 00:45:08 -0300

    config: add global validation pipe in boostrap function to start validating dtos, add swagger document builder code to create api html doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Wefit backend')
+    .setDescription(`The wefit backend api's description`)
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
